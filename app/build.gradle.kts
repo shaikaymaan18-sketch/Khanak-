23000256baa8a71fb7ffa784a39c35e93eb2fcc1
shaikaymaan18-sketch <shaikaymaan18@gmail.com>
2026-09-22 20:34:23 +0530

    Add xfetch2 library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,5 +69,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("com.google.code.gson:gson:2.10.1")
+implementation("androidx.tonyodev.fetch2:xfetch2:3.1.6")
+
 }
 
